8247c67373fe5935f9712bcb7aa7624dd3bd5404
mughidaf <74787238+mughidaf@users.noreply.github.com>
2023-01-16 10:07:35 +0700

    Add files via upload

---
+++ storage/framework/views/5ccecdfee52899c67a9be4595e3c4c7036791533.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah tempat</title>
+    <style>
+        form {
+        width: 500px;
+        margin: 0 auto;
+        text-align: left;
+        }
+
+        label {
+        display: block;
+        margin-bottom: 8px;
+        }
+
+        input[type="text"],
+        textarea,
+        input[type="number"],
+        input[type="file"],
+        input[type="submit"] {
+        width: 100%;
+        padding: 12px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        resize: vertical;
+        }
+
+        input[type="submit"] {
+        background-color: #4CAF50;
+        color: white;
+        cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+        background-color: #45a049;
+        }
+
+        .error{
+            color: red;
+        }
+
+    </style>
+
+    <body>
+        <body>
+            <form action="/Konser" method="post" enctype="multipart/form-data">
+                <?php echo csrf_field(); ?>
+                <label for="kategori_id">ID Kategori:</label><br>
+                <?php $__errorArgs = ['kategori_id'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="number" id="kategori_id" name="kategori_id" min="1" max="8"><br>
+                <br>
+                <label for="name">Nama Tempat:</label><br>
+                <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="text" id="name" name="name"><br>
+                <br>
+                <label for="desc">Deskripsi Tempat:</label><br>
+                <?php $__errorArgs = ['desc'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <textarea id="desc" name="desc"></textarea><br>
+                <br>
+                <label for="alamat">Alamat:</label><br>
+                <?php $__errorArgs = ['alamat'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="text" id="alamat" name="alamat"><br>
+                <br>
+                <label for="harga">Harga:</label><br>
+                <?php $__errorArgs = ['harga'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="number" id="harga" name="harga"><br>
+                <br>
+                <label for="rating">Rating:</label><br>
+                <?php $__errorArgs = ['rating'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="number" id="rating" name="rating" min="0" max="5"><br>
+                <br>
+                <label for="img">Image:</label><br>
+                <?php $__errorArgs = ['img'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="file" id="img" name="img"><br>
+                <br>
+                <label for="telp">Telp:</label><br>
+                <?php $__errorArgs = ['telp'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="text" id="telp" name="telp"><br>
+                <br>
+                <label for="gmaps">Link Gmaps:</label><br>
+                <?php $__errorArgs = ['gmaps'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <div class="error">
+                        <?php echo e($message); ?>
+
+                    </div>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <input type="text" id="gmaps" name="gmaps"><br>
+                <br>
+                <input type="submit" value="Submit">
+              </form>
+        </body>
+    </body>
+
+</head>
+<body>
+    
+</body>
+</html><?php /**PATH C:\Ghiyats\Kuliah\Semester 5\RPL Impal\Tugas\Rentify\resources\views/Form.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/9320200748ba97d49c9bd8b18ec71ca8464c6d94.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Halaman Postingan</title>
+  <style>
+    header {
+    background-color: lightblue;
+    text-align: center;
+    padding: 20px;
+    }
+
+    section {
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    }
+
+  </style>
+</head>
+<body>
+  <header>
+    <h1>Judul Halaman</h1>
+  </header>
+  <section>
+    <h2>Sub Judul</h2>
+    <p>Pesan isi post</p>
+  </section>
+</body>
+</html>
+<?php /**PATH C:\Ghiyats\Kuliah\Semester 5\RPL Impal\Tugas\Rentify\resources\views//showBantuan.blade.php ENDPATH**/ ?>
